0edb1d3fb52b92c1e649060d7458dc22816402c7
Jimena Beltran Lazo <jbeltranlazo3@gmail.com>
2025-08-12 21:45:36 -0300

    Trabajo Práctico TaTeTi - Jimena Beltran Lazo - Computación I

--- main.py
+++ main.py
@@ -1,49 +1,6 @@
-# Interfaz Tateti
+# Juego TaTeTi
 
-from src.tateti import Tateti
-from src.jugador import Jugador
-from src.tablero import PosOcupadaException
-
-def main():
-    print("¡Bienvenidos al Tateti!")
-
-    nombre1 = input("Ingrese nombre del jugador X: ")
-    nombre2 = input("Ingrese nombre del jugador O: ")
-    j1 = Jugador(nombre1, "X")
-    j2 = Jugador(nombre2, "O")
-
-    juego = Tateti(j1, j2)
-
-    while True:
-        print("\nTablero:")
-        juego.tablero.mostrar_tablero()
-        jugador = juego.jugador_actual()
-        print(f"Turno de {jugador.nombre} ({jugador.ficha})")
-
-        try:
-            fila = int(input("Ingrese fila (0-2): "))
-            columna = int(input("Ingrese columna (0-2): "))
-            juego.ocupar_casilla(fila, columna)
-        
-            # Evaluar ganador
-            if juego.tablero.hay_ganador(jugador.ficha):
-                juego.tablero.mostrar_tablero()
-                print(f"¡Felicidades {jugador.nombre}! Ganaste el juego.")
-                break
-
-            # Evaluar empate
-            if juego.tablero.contenedor_lleno():
-                juego.tablero.mostrar_tablero()
-                print("¡Es un empate! No quedan más movimientos.")
-                break
-
-        except ValueError:
-            print("Debe ingresar números válidos.")
-        except PosOcupadaException as e:
-            print(e)
-        except IndexError:
-            print("Fila o columna fuera de rango.")
+from src.juego import JUGAR
 
 if __name__ == '__main__':
-    main()
-    
+    JUGAR()
---
+++ src/juego.py
@@ -0,0 +1,44 @@
+# Interfaz TaTeTi
+
+from .tateti import Tateti
+from .jugador import Jugador
+from .tablero import PosOcupadaException
+
+def JUGAR():
+    print("¡Bienvenidos al Tateti!")
+
+    nombre1 = input("Ingrese nombre del jugador X: ")
+    nombre2 = input("Ingrese nombre del jugador O: ")
+    j1 = Jugador(nombre1, "X")
+    j2 = Jugador(nombre2, "O")
+
+    juego = Tateti(j1, j2)
+
+    while True:
+        print("\nTablero:")
+        juego.tablero.mostrar_tablero()
+        jugador = juego.jugador_actual()
+        print(f"Turno de {jugador.nombre} ({jugador.ficha})")
+        
+        try:
+            fila = int(input("Ingrese fila (0-2): "))
+            columna = int(input("Ingrese columna (0-2): "))
+            juego.ocupar_casilla(fila, columna)
+            
+            # Evaluar Ganador
+            if juego.tablero.hay_ganador(jugador.ficha):
+                juego.tablero.mostrar_tablero()
+                print(f"¡Felicidades {jugador.nombre}! Ganaste el juego.")
+                break
+            # Evaluar Empate
+            if juego.tablero.contenedor_lleno():
+                juego.tablero.mostrar_tablero()
+                print("¡Es un empate! No quedan más movimientos.")
+                break
+
+        except ValueError:
+            print("Debes ingresar números válidos.")
+        except PosOcupadaException as e:
+            print(e)
+        except IndexError:
+            print("Fila o columna fuera de rango.")
